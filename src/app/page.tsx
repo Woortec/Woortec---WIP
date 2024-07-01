4e022438d4c38bb1ee63cc4557fb2fb590d24bf8
Miguel <miguelgasang@gmail.com>
2024-07-01 17:59:37 +0800

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,5 @@
-'use client'
+import { redirect } from 'next/navigation';
 
-// pages/redirect.tsx
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-
-declare global {
-  interface Window {
-    gtag: (...args: any[]) => void;
-  }
-}
-
-export default function RedirectPage(): void {
-  const router = useRouter();
-
-  useEffect(() => {
-    // Push the initial pageview event to GTM
-    if (typeof window !== 'undefined' && window.gtag) {
-      window.gtag('config', 'GTM-NXB5KPF3', {
-        page_path: window.location.pathname,
-      });
-    }
-
-    // Perform the redirect
-    router.push('/dashboard');
-  }, [router]);
-
+export default function Page(): never {
+  redirect('/dashboard');
 }
