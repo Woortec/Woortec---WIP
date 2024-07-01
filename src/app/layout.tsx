241f83a0e42ceb3b8c98e4ea402d871cae5b8048
Miguel <miguelgasang@gmail.com>
2024-07-01 16:39:46 +0800

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,9 @@
+'use client'
+
 import * as React from 'react';
 import type { Viewport } from 'next';
+import { useEffect } from 'react';
+import insertGTM from '../../utils/insertGTM'; // Adjust the path as needed
 
 import '@/styles/global.css';
 
@@ -13,10 +17,24 @@ interface LayoutProps {
   children: React.ReactNode;
 }
 
+const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
+
 export default function Layout({ children }: LayoutProps): React.JSX.Element {
+  useEffect(() => {
+    if (GTM_ID) {
+      insertGTM(GTM_ID);
+    }
+  }, []);
+
   return (
     <html lang="en">
+      <head>
+        {/* Add any other head elements here */}
+      </head>
       <body>
+        <noscript>
+          <iframe src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`} height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe>
+        </noscript>
         <LocalizationProvider>
           <UserProvider>
             <ThemeProvider>{children}</ThemeProvider>
