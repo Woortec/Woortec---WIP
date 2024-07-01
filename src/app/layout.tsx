70b62156a04bbe591abffa27dae70cfc84c6bc93
Miguel <miguelgasang@gmail.com>
2024-07-01 17:57:27 +0800

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,5 @@
-'use client'
-
 import * as React from 'react';
 import type { Viewport } from 'next';
-import { useEffect } from 'react';
 
 import '@/styles/global.css';
 
@@ -16,34 +13,10 @@ interface LayoutProps {
   children: React.ReactNode;
 }
 
-const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID || 'GTM-NXB5KPF3';
-
 export default function Layout({ children }: LayoutProps): React.JSX.Element {
-  useEffect(() => {
-    if (GTM_ID) {
-    }
-  }, []);
-
   return (
     <html lang="en">
-      <head>
-        {/* Google Tag Manager */}
-        <script dangerouslySetInnerHTML={{ __html: `
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','${GTM_ID}');
-        ` }} />
-        {/* End Google Tag Manager */}
-        {/* Add any other head elements here */}
-      </head>
       <body>
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`} height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe>
-        </noscript>
-        {/* End Google Tag Manager (noscript) */}
         <LocalizationProvider>
           <UserProvider>
             <ThemeProvider>{children}</ThemeProvider>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,26 @@
-import { redirect } from 'next/navigation';
+// pages/redirect.tsx
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
-export default function Page(): never {
-  redirect('/dashboard');
+declare global {
+  interface Window {
+    gtag: (...args: any[]) => void;
+  }
+}
+
+export default function RedirectPage(): void {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Push the initial pageview event to GTM
+    if (typeof window !== 'undefined' && window.gtag) {
+      window.gtag('config', 'GTM-NXB5KPF3', {
+        page_path: window.location.pathname,
+      });
+    }
+
+    // Perform the redirect
+    router.push('/dashboard');
+  }, [router]);
+
 }
