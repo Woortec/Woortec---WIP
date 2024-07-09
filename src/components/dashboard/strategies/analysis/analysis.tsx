7105ee7f2388d1e9ab7d2ae512f4fb156b84013f
Miguel <miguelgasang@gmail.com>
2024-07-09 20:14:09 +0800

    Fixed Ads Strategies

--- src/components/dashboard/strategies/analysis/analysis.tsx
+++ src/components/dashboard/strategies/analysis/analysis.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 // src/file-saver.d.ts
 declare module 'file-saver' {
   export function saveAs(data: Blob | string, filename?: string, options?: any): void;
--- src/components/dashboard/strategies/optimization/optimization.tsx
+++ src/components/dashboard/strategies/optimization/optimization.tsx
@@ -1,4 +1,6 @@
-// src/file-saver.d.ts
+'use client';
+
+
 declare module 'file-saver' {
   export function saveAs(data: Blob | string, filename?: string, options?: any): void;
 }
