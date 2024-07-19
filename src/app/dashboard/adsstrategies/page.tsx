bd5128addf7fe2703a2e8f6bfaf002039caeb39c
Miguel <miguelgasang@gmail.com>
2024-07-19 17:58:58 +0800

    create campaign + middleware

--- src/app/dashboard/adsstrategies/page.tsx
+++ src/app/dashboard/adsstrategies/page.tsx
@@ -1,17 +1,27 @@
-'use client'
+'use client';
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import CampaignSetup from '../../../components/dashboard/adsstrategies/adsstrategies';  // Adjust the import path as necessary
 import Preparing from '../../../components/dashboard/adsstrategies/preparing';
 import Results from '../../../components/dashboard/adsstrategies/results';
 
 function App() {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) {
+    return null; // Render nothing on the server side
+  }
+
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/dashboard/CampaignSetup" element={<CampaignSetup />} />
+          <Route path="/dashboard/adsstrategies" element={<CampaignSetup />} />
           <Route path="/dashboard/preparing" element={<Preparing />} />
           <Route path="/dashboard/results" element={<Results />} />
         </Routes>
