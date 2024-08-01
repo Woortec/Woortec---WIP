e835c1e5e1cae71f554dfd26aa8a2dce5b183e5b
Miguel <miguelgasang@gmail.com>
2024-08-01 20:45:19 +0800

    front-end (campaign setup)

--- src/components/dashboard/layout/config.ts
+++ src/components/dashboard/layout/config.ts
@@ -5,7 +5,6 @@ export const navItems = [
   { key: 'overview', title: 'Overview', href: paths.dashboard.overview, icon: 'chart-bar' },
   { key: 'performance', title: 'Ads Performance', href: paths.dashboard.performance, icon: 'trendup' },
   { key: 'connection', title: 'Social Connections', href: paths.dashboard.connection, icon: 'broadcast' },
-  { key: 'strategies', title: 'Campaign Setup', href: paths.dashboard.strategies, icon: 'usergear' },
   { key: 'campaign', title: 'Campaign Setup', href: paths.dashboard.campaign, icon: 'usergear' },
   { key: 'subscription', title: 'Subscription', href: paths.dashboard.subscription, icon: 'shoppingbag' },
   { key: 'adsstrategies', title: 'Ads Strategies', href: paths.dashboard.adsstrategies, icon: 'strategy' },
