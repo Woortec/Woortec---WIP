908bccb95972dfae3c26933f4478aed4cf74c287
Miguel <154050165+angeldust1220@users.noreply.github.com>
2024-10-01 21:41:10 +0800

    test

--- src/types/nav.d.ts
+++ src/types/nav.d.ts
@@ -1,12 +1,15 @@
-// src/types/nav.ts
 export interface NavItemConfig {
   key: string;
-  title: string;
+  title?: string;
   disabled?: boolean;
   external?: boolean;
   label?: string;
   icon?: string;
-  href: string;
+  href?: string;
   items?: NavItemConfig[];
-  matcher?: (pathname: string) => boolean;
+  // Matcher cannot be a function in order
+  // to be able to use it on the server.
+  // If you need to match multiple paths,
+  // can extend it to accept multiple matchers.
+  matcher?: { type: 'startsWith' | 'equals'; href: string };
 }
