873337eee8a1cedcad56b9fb342ec9c29bdbfcb8
Miguel <miguelgasang@gmail.com>
2024-11-19 16:27:41 +0800

    test

--- src/components/auth/guest-guard.tsx
+++ src/components/auth/guest-guard.tsx
@@ -28,7 +28,7 @@ export function GuestGuard({ children }: GuestGuardProps): React.JSX.Element | n
 
     if (user) {
       logger.debug('[GuestGuard]: User is logged in, redirecting to /dashboard');
-      router.push('/dashboarsd');
+      router.push('/dashboard');
       return;
     }
 
