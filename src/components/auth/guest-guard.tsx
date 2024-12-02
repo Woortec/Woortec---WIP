41f34df7696c6a910a3d03a7193b46e83908a3c9
Miguel <miguelgasang@gmail.com>
2024-12-02 18:33:05 +0800

    facebook login

--- src/components/auth/guest-guard.tsx
+++ src/components/auth/guest-guard.tsx
@@ -4,6 +4,7 @@ import * as React from 'react';
 import { useRouter } from 'next/navigation';
 import Alert from '@mui/material/Alert';
 
+import { paths } from '@/paths';
 import { logger } from '@/lib/default-logger';
 import { useUser } from '@/hooks/use-user';
 
@@ -27,8 +28,8 @@ export function GuestGuard({ children }: GuestGuardProps): React.JSX.Element | n
     }
 
     if (user) {
-      logger.debug('[GuestGuard]: User is logged in, redirecting to /dashboard');
-      router.push('/dashboard');
+      logger.debug('[GuestGuard]: User is logged in, redirecting to dashboard');
+      router.replace(paths.dashboard.overview);
       return;
     }
 
