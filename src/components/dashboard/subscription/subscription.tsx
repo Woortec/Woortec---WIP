a459b35d5e0ef08a8420f902a3b2db7f97fec9d0
Miguel <miguelgasang@gmail.com>
2025-03-24 17:31:59 +0800

    Stripe Test

--- src/components/dashboard/subscription/subscription.tsx
+++ src/components/dashboard/subscription/subscription.tsx
@@ -42,7 +42,7 @@ const Subscription = () => {
 
   const plans = [
     {
-      id: isYearly ? 'price_1QDjDqHow0UPMFTyFVrpzNPc' : 'price_1QDjDqHow0UPMFTyFVrpzNPc',
+      id: isYearly ? 'price_1P90eAHow0UPMFTymffkim7t' : 'price_1P90eAHow0UPMFTyqjjSmILe',
       name: 'Basic',
       price: isYearly ? '$299.99/year' : '$29.99/month',
       features: [
