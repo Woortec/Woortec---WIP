04d89369912b828ee1bf00b6b679b7d2225cccd6
Miguel <miguelgasang@gmail.com>
2025-05-07 05:54:44 +0800

    Ads Strategies

---
+++ src/app/dashboard/strategy/strategycreation/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import React from 'react';
+import StrategyCreationPage from '@/components/dashboard/strategy-creation/setup02';
+
+function App() {
+  return (
+    <div className="App">
+      <StrategyCreationPage />
+    </div>
+  );
+}
+
+export default App;
