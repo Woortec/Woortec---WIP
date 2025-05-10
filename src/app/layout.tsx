bc7eef4b4eed413e2b0aa21012ca1f792814b131
Miguel <miguelgasang@gmail.com>
2025-05-11 07:11:25 +0800

    google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+export const dynamic = 'force-dynamic';
 import * as React from 'react'
 import { usePathname, useSearchParams } from 'next/navigation'
 import { TourProvider } from '@/contexts/TourContext'
